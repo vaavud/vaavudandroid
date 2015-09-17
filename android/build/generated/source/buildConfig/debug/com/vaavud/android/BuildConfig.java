ee822f92125811b4bceee857a94d57832867018e
root <root@localhost.localdomain>
2015-09-17 08:08:58 -0500

    a

---
+++ android/build/generated/source/buildConfig/debug/com/vaavud/android/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.vaavud.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.vaavud.android";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 20150915;
+  public static final String VERSION_NAME = "";
+}
